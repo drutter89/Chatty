bc509a758bfe0eb3735e9f2a19efdc39d789259f
Dylan Rutter <dylanmrutter89@gmail.com>
2019-01-28 18:11:38 +0000

    got my components set up and in their own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import React, {Component} from 'react';
+import ChatBar from "./ChatBar.jsx";
+import MessageList from "./MessageList.jsx";
 
 class App extends Component {
   render() {
     return (
       <div>
         <nav className="navbar">
-          <a href="/" classNameName="navbar-brand">Chatty</a>
+          <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <main className="messages">
-          <div className="message">
-            <span className="message-username">Anonymous1</span>
-            <span className="message-content">I won't be impressed with technology until I can download food.</span>
-          </div>
-          <div className="message system">
-          Anonymous1 changed their name to nomnom.
-          </div>
-        </main>
-        <footer className="chatbar">
-          <input className="chatbar-username" placeholder="Your Name (Optional)" />
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        </footer>       
+        <ChatBar />
+        <MessageList />
+  
+  
+  
+       
       </div>
     )
   }
 }
 export default App;
+
+
+
+
+
+
+
+
+
+
